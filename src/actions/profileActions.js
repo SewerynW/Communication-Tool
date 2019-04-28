8cb36452ed782007d30adcf587ba395956ee56ad
Anna Olender <annkiaolender@gmail.com>
2019-04-28 15:33:57 +0200

    Deleting console.logs

--- src/actions/profileActions.js
+++ src/actions/profileActions.js
@@ -7,19 +7,15 @@ export const REMOVE_DATA = "Remove_Data";
 export const FETCH_PROFILE = "Fetch_Profile";
 
 
-
-
-//przesyłam obiekt profilu
-
 export const editProfile = profile => dispatch => {
     const tempProfile={
         Name: profile.Name,
         GivenName: profile.Surname,
         Photo: profile.Photo
+};
 
-    };
-    const tempPhoto = profile.Photo || null;
-    const formData= new FormData();
+const tempPhoto = profile.Photo || null;
+const formData= new FormData();
     if(tempPhoto){
         formData.append('photo', tempPhoto);
     }
@@ -27,43 +23,40 @@ export const editProfile = profile => dispatch => {
     return Axios.updateUserProfile(formData)
         .then(response => 
             dispatch (editProfileSuccess(response.data)
-            ));
+        ));
 }
 
 
-
 const editProfileSuccess = ({ Name, Surname, Photo }) => ({
-
     type: EDIT_PROFILE,
     payload: {
         Name,
         Surname,
         Photo
-    }
-  });
+     }
+});
 
   const removeProfileSuccess = () => ({
     type: REMOVE_PROFILE
   });
 
 
-  export const removeProfile = () => dispatch => {
-   //   console.log("Hello");
+export const removeProfile = () => dispatch => {
       return Axios.deleteUserProfile().then(response => {
           console.log(response);
           dispatch(removeProfileSuccess()
-        )})};
+)})};
  
-  export const fetchProfile = () =>  dispatch => 
+export const fetchProfile = () =>  dispatch => 
      Axios.getInfoAboutUser()
         .then(response => {
             dispatch(fetchProfileSuccess(response))})
         .catch(error => {throw error});
 
-    const fetchProfileSuccess = user => ({
+const fetchProfileSuccess = user => ({
         type: FETCH_PROFILE,
         payload: user
-          })
+     })
 
 
 
