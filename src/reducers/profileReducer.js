3d28b435995373165297b9f84fa8f36ed9d24dd7
Anna <annkiaolender@gmail.com>
2019-04-29 17:22:53 +0200

    Commit before merging

--- src/reducers/profileReducer.js
+++ src/reducers/profileReducer.js
@@ -9,33 +9,28 @@ const initState = {
   userProfile: {}
 };
 
-  export const profileReducer = (state = initState, action) => {
-    switch (action.type) {
-      case EDIT_PROFILE:
-        return {
-          userProfile: { ...state.userProfile, 
-            Name:action.Name,
-            GivenName:action.GivenName,
-          }
-        };
-      case REMOVE_PROFILE:
-        return {
-          userProfile:{},
-          state: undefined
-
-        };
-     case FETCH_PROFILE:
-     return{
-            Name:action.payload.Name,
-            GivenName:action.payload.GivenName,
-            Photo:action.payload.Photo
-     };
-     case REMOVE_DATA:
-    return{
-      state:null
-    }
-      default:
-        return state;
-    }
-  };
-  
+export const profileReducer = (state = initState, action) => {
+  switch (action.type) {
+    case EDIT_PROFILE:
+      return {
+        ...state.userProfile,
+        Name: action.payload.Name,
+        GivenName: action.payload.GivenName,
+        Photo: action.payload.Photo
+      };
+    case REMOVE_PROFILE:
+      return {
+        userPosts: null
+      };
+    case FETCH_PROFILE:
+      return {
+        ...action.payload
+      };
+    case REMOVE_DATA:
+      return {
+        state: null
+      };
+    default:
+      return state;
+  }
+};
